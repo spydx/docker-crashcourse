8b4bffd310fe3cd96b984413a9654ac37b804c66
Kenneth <fossen.kenneth@gmail.com>
2021-02-16 23:21:59 +0100

    upsi new WebAPI

--- WeatherApi/Startup.cs
+++ WeatherApi/Startup.cs
@@ -1,10 +1,15 @@
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace WeatherApi
@@ -27,7 +32,6 @@ namespace WeatherApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WeatherApi", Version = "v1" });
             });
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,7 +44,8 @@ namespace WeatherApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WeatherApi v1"));
             }
 
-        
+            app.UseHttpsRedirection();
+
             app.UseRouting();
 
             app.UseAuthorization();
