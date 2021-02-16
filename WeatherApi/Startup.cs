3f548b2c7ec5a64e860743891b59966d9b9c70ec
Kenneth <fossen.kenneth@gmail.com>
2021-02-16 23:23:36 +0100

    Removed HTTPS binding

--- WeatherApi/Startup.cs
+++ WeatherApi/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+
 using Microsoft.OpenApi.Models;
 
 namespace WeatherApi
@@ -44,7 +38,6 @@ namespace WeatherApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WeatherApi v1"));
             }
 
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
